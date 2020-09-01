a8994d1cd971a7db5643c6ecb0b822c474c75f63
kilimill <kilimill2585@gmail.com>
2020-09-01 20:39:58 +0700

    start

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function(){
+
+  $('.about__wrapper').slick({
+    dots: true,
+    nextArrow: '<button class="slider-arrow slider-right"><img src="images/arrow-right.svg" alt=""></button>',
+    prevArrow: '<button class="slider-arrow slider-left"><img src="images/arrow-left.svg" alt=""></button>',
+    fade: true,
+    speed: 500,
+    infinite: true
+  });
+
+
+});
