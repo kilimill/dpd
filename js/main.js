2e8c02310b28131eb05547bc90615d4e6257e219
kilimill <kilimill2585@gmail.com>
2020-09-02 18:18:52 +0700

    fixed

--- js/main.js
+++ js/main.js
@@ -9,5 +9,70 @@ $(function(){
     infinite: true
   });
 
+  $('.slider__inner').slick({
+    
+    slidesToShow: 2,
+    dots: true,
+    nextArrow: '<button class="slider-arrow slider-right"><img src="images/arrow-right.svg" alt=""></button>',
+    prevArrow: '<button class="slider-arrow slider-left"><img src="images/arrow-left.svg" alt=""></button>',
+    infinite: true,
+  });
+
+
+
+  $('.wrapper .tab').on('click', function (event) {
+    var id = $(this).attr('data-id');
+    $('.wrapper').find('.tab-item').removeClass('active-tab').hide();
+    $('.wrapper .tabs').find('.tab').removeClass('active');
+    $(this).addClass('active');
+    $('#' + id).addClass('active-tab').fadeIn();
+    return false;
+  });
+
+
+  $('.chart-wrapper .chart-tab').on('click', function (event) {
+    var id = $(this).attr('data-id');
+    $('.chart-wrapper').find('.chart__tab-item').removeClass('chart__active-tab').hide();
+    $('.chart-wrapper .chart-tabs').find('.chart-tab').removeClass('chart__active');
+    $(this).addClass('chart__active');
+    $('#' + id).addClass('chart__active-tab').fadeIn();
+    return false;
+  });
+
+  $('.popular__inner').slick({
+    slidesToShow: 1,
+    dots: true,
+    nextArrow: '<button class="slider-arrow slider-right"><img src="images/arrow-right.svg" alt=""></button>',
+    prevArrow: '<button class="slider-arrow slider-left"><img src="images/arrow-left.svg" alt=""></button>',
+    infinite: true,
+  });
+  
+  $('.tabs-links__list').on('click', function () {
+    
+  });
+
+  $('.tabs-links__list-item').on('click', function () {
+
+    $('.tabs-links__list').removeClass('tabs-links__list--active');
+    $(this).parent().addClass('tabs-links__list--active');
+
+  });
 
 });
+
+var links = document.querySelectorAll('.tabs-links li');
+var content = document.querySelectorAll('.tabs-content li');
+for (var i = 0; i < links.length; i++) {
+  (function (i) {
+    var link = links[i];
+    link.onclick = function () {
+      for (var j = 0; j < content.length; j++) {
+        var opacity = window.getComputedStyle(content[j]).opacity;
+        if (opacity == "1") {
+          content[j].style.opacity = "0";
+        }
+      }
+      content[i].style.opacity = "1";
+    }
+  })(i);
+}
